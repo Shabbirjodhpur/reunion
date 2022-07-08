3564c18503f71031b0b8f728307fd2a3e059235c
shabbir_j <shabbirjodhpurwala@gmail.com>
2022-07-08 11:01:19 +0530

    final

--- makeqr.js
+++ makeqr.js
@@ -4,15 +4,15 @@ const data = fs.readFileSync('./data.txt','UTF-8')
 
 const teams = data.split('\r\n')
 
-function makeQRCode(path,hash){
+function makeQRCode(path,hash,i){
 	path = `https://shabbirjodhpur.github.io/reunion/${path}`
 	var qr_type = qr.image(`${path}`, { type: 'png' });
-	qr_type.pipe(require('fs').createWriteStream(`./images/${hash}.png`));
+	qr_type.pipe(require('fs').createWriteStream(`./images/${i}.png`));
  
 	var type_string = qr.imageSync(`${path}`, { type: 'png' });
 }
 for(let i=0;i<teams.length;i++){
 	const metrics = teams[i].split(',')
 
-	makeQRCode(`files/${metrics[2]}.html`,metrics[3])
+	makeQRCode(`files/${metrics[2]}.html`,metrics[3],i)
 }
